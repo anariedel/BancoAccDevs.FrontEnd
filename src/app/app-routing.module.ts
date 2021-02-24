02aaa50f37f513904bd55dbd6dfc83df3c368f9a
Flavio <flaviosantana.c@gmail.com>
2021-02-24 18:53:31 -0300

    feat: layout inicial para home, rota e fonte

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+
+const routes: Routes = [
+  { path: 'home', 
+    loadChildren: () => import('./home/home.module').then(m => m.HomeModule),
+  }, 
+  { path: '', redirectTo: 'home', pathMatch: 'full'},
+  { path: '**', component: HomeComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
