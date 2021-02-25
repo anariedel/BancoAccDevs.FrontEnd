c753f90cecb8581717bf1cc655cce7562908c504
helderlim <helderfelima16@gmail.com>
2021-02-25 17:06:51 -0300

    feat: create esqueci minha senha component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
+import { RecoveryPassComponent } from './recovery-pass/recovery-pass.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    RecoveryPassComponent,
   ],
   imports: [
     BrowserModule,
--- src/app/login/login-routing.module.ts
+++ src/app/login/login-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { RecoveryPassComponent } from '../recovery-pass/recovery-pass.component';
 import { LoginComponent } from './login.component';
 
 const routes: Routes = [{
   path: '',
   component: LoginComponent
+},{
+  path: 'recoverypass',
+  component: RecoveryPassComponent
 }];
 
 @NgModule({
