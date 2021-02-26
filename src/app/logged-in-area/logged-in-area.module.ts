60eca1f9e9e8a3d8f00d7a42444e7cdf955512d6
Flávio Santana <flaviosantana.c@gmail.com>
2021-02-25 23:57:45 -0300

    feat: estrutura para módulo área logada e dash

---
+++ src/app/logged-in-area/logged-in-area.module.ts
@@ -0,0 +1,21 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+
+import { SharedModule } from '../shared/shared.module';
+import { DashComponent } from './dash/dash.component';
+import { LoggedInAreaRoutingModule } from './logged-in-area-routing.module';
+import { LoggedInAreaComponent } from './logged-in-area.component';
+
+
+@NgModule({
+  declarations: [
+    LoggedInAreaComponent,
+    DashComponent
+  ],
+  imports: [
+    CommonModule,
+    LoggedInAreaRoutingModule,
+    SharedModule
+  ]
+})
+export class LoggedInAreaModule { }
