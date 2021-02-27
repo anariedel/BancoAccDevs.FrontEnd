dbdf3b221187cf12210f3d187cfbf8ccdd36aa24
anariedel <luisariedel@yahoo.com.br>
2021-02-27 12:13:07 -0300

    fix: environment links

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  API_URL: 'https://accenture-java-desafio.herokuapp.com/v2/api-docs',
+  API_URL: 'https://accenture-java-desafio.herokuapp.com/',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  API_URL: 'https://accenture-java-desafio.herokuapp.com/v2/api-docs',
+  API_URL: 'https://accenture-java-desafio.herokuapp.com/',
 };
 
 /*
