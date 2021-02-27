03d688c847a2048fcd9db828a93cbb68ff481785
Nathan Castro <nathanslc18@gmail.com>
2021-02-27 18:47:07 -0300

    git commit -m "edição do Form-1"

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-home',
+  selector: 'app-home', 
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
+  
 })
 export class HomeComponent implements OnInit {
 
+  CPF: number;
+  usuario: string;
+  nomeCompleto: Text;
+  password: string;
+
+
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit(){
+  }
+
+  onSubmit(form){
+    if(!form.valid){
+      alert('Formulário Iválido')
+    }
+    
+    
+    
+    
+    console.log('CPF', this.CPF);
+    console.log('usuario', this.usuario);
+    console.log('nome do usuario', this.nomeCompleto);
+    console.log('Senah', this.password);
+
   }
 
 }
