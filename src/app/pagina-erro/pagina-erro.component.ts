e4e66abd9da0fd69da8a0a3fc8894774afe44826
anariedel <luisariedel@yahoo.com.br>
2021-02-27 21:18:53 -0300

    "feat: página de erro"

---
+++ src/app/pagina-erro/pagina-erro.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pagina-erro',
+  templateUrl: './pagina-erro.component.html',
+  styleUrls: ['./pagina-erro.component.scss']
+})
+export class PaginaErroComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  redirecionar() {
+    
+  }
+
+}
