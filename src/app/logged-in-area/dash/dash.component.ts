c27bca093b8f9e2511eec8c131e855c181cace2a
Flávio Santana <flaviosantana.c@gmail.com>
2021-03-01 17:20:50 -0300

    feat: carga de dados da dashboard
    * feat: chamada a api dashboard
    
    * feat: estrutura base de exibição
    
    * Merge branch 'master' into feature/data-dash
    
    * fix: utilizando api service
    
    * feat: saldo conta

--- src/app/logged-in-area/dash/dash.component.ts
+++ src/app/logged-in-area/dash/dash.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+import { Conta } from 'src/app/interfaces/conta.interface';
+import { Dashboard } from 'src/app/interfaces/dashboard.interface';
+import { Lancamento } from 'src/app/interfaces/lancamento.interface';
+import { AuthService } from 'src/app/shared/services/auth.service';
+
+import { DashService } from './dash.service';
 
 @Component({
   selector: 'app-dash',
@@ -7,9 +14,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashComponent implements OnInit {
 
-  constructor() { }
+  contaBanco: Conta;
+  contaCredito: Conta;
+  lancamentos: Array<Lancamento>;
+
+  filtroDataInicial = "2020-01-01";
+  filtroDataFinal = "2021-12-31";
+  
+  constructor(
+    private dashService: DashService,
+    private authService: AuthService,
+    private toastr: ToastrService
+  ) { }
 
   ngOnInit(): void {
+    this.loadDashData();
   }
 
+
+
+  loadDashData() {
+    this.dashService.getDash(
+      this.filtroDataFinal, 
+      this.filtroDataInicial, 
+      this.authService.getSessao().usuario.login)
+      .subscribe(
+        response => this.onSuccessGetDash(response),
+        error => this.onErrorGetDash(error),
+      )
+  }
+
+  onSuccessGetDash(response: Dashboard) {
+    this.contaBanco = response.contaBanco;
+    this.contaCredito = response.contaCredito;
+    this.lancamentos = [...response.contaCredito.lancamentos, ...response.contaBanco.lancamentos];
+  }
+
+  onErrorGetDash(error: any) {
+    this.toastr.error('Erro ao obter informações','Erro');
+  }
 }
