57defac92fdfcda0a593cecb3662e142a25d63c3
Flavio <flaviosantana.c@gmail.com>
2021-03-01 13:43:39 -0300

    feat: utilizando api service

--- src/app/login/login.service.ts
+++ src/app/login/login.service.ts
@@ -1,4 +1,3 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { tap } from 'rxjs/operators';
@@ -6,6 +5,7 @@ import { environment } from 'src/environments/environment';
 
 import { Login } from '../interfaces/login.interface';
 import { Sessao } from '../interfaces/sessao.interface';
+import { ApiService } from '../shared/services/api.service';
 import { AuthService } from '../shared/services/auth.service';
 
 @Injectable({
@@ -16,12 +16,12 @@ export class LoginService {
   API_URL = environment.API_URL;
 
   constructor(
-    private http: HttpClient,
+    private api: ApiService,
     private authService: AuthService
     ) { }
 
   login(login: Login): Observable<Sessao> {
-    return this.http.post<Sessao>(`${this.API_URL}/login`, login)
+    return this.api.post(`${this.API_URL}/login`, login)
       .pipe(
         tap(response => {
           this.authService.setSessao(response);
